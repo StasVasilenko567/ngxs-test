a578287f33f0ae20c3180e73b81e2767d035ef7a
StasVasilenko567 <vasilenko.fortech@gmail.com>
2025-02-14 12:12:52 +0300

    fix

--- src/app/pages/blogpage/guards/state-exist.guard.ts
+++ src/app/pages/blogpage/guards/state-exist.guard.ts
@@ -1,11 +1,15 @@
 import { inject } from "@angular/core";
 import { Store } from "@ngxs/store";
 import { BlogSelectors } from "../store/blog.selectors";
+import { Router } from "@angular/router";
 
 export const stateExistGuard = () => {
     const store = inject(Store);
+    const router = inject(Router);
 
     const sig = store.selectSignal(BlogSelectors.blogs);
 
-    return sig().length > 0;
+    if (sig().length === 0) {
+        router.navigate(['/']);
+    }
 }
